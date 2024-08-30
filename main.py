b9c8d6ad02ac7915e27094361e49a85dec456043
mohsen159 <108313366+mohsen159@users.noreply.github.com>
2024-08-30 02:59:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from telegram import Bot
 # Telegram bot token and chat ID
 TELEGRAM_TOKEN = "7487022753:AAFPHijXrk4EqTmNJxhHTT6MllmTHog5Ue8"
 CHAT_ID = "922909522"  # Replace with your actual chat ID
-
 # Website URL to check
 WEBSITE_URL = "https://expressedzshop.peasy.top/body-scale-03-ar.html?fbclid=IwY2xjawE9_jpleHRuA2FlbQIxMAABHWYS7AjEfQZkjSrNw7JKMIUyH436kXYh2HpzhrkaE9jX5_WFUmQivSW_oA_aem_qRttjfua-CNfRG5NGm6Yag"  # Replace with the URL you want to monitor
-
 def check_website(url):
     """Check if the website is reachable."""
     try:
@@ -22,9 +20,10 @@ def send_telegram_message(token, chat_id, message):
     bot.send_message(chat_id=chat_id, text=message)
 
 def main():
-    if not check_website(WEBSITE_URL):
-        message = f"Alert: The website {WEBSITE_URL} is down!"
-        send_telegram_message(TELEGRAM_TOKEN, CHAT_ID, message)
+    website_status = check_website(WEBSITE_URL)
+    status_message = "up" if website_status else "down"
+    message = f"Alert: The website {WEBSITE_URL} is {status_message}!"
+    send_telegram_message(TELEGRAM_TOKEN, CHAT_ID, message)
 
 if __name__ == "__main__":
     main()
