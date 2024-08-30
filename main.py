a36452fa2d3ea0fb9d7d7d2298eed5977acf26a8
mohsen159 <108313366+mohsen159@users.noreply.github.com>
2024-08-30 03:09:13 +0100

    Update and rename check_website.py to main.py

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
 import requests
 from telegram import Bot
 
-# Telegram bot token and chat ID
 TELEGRAM_TOKEN = "7487022753:AAFPHijXrk4EqTmNJxhHTT6MllmTHog5Ue8"
-CHAT_ID = "922909522"  # Replace with your actual chat ID
-# Website URL to check
-WEBSITE_URL = "https://expressedzshop.peasy.top/body-scale-03-ar.html?fbclid=IwY2xjawE9_jpleHRuA2FlbQIxMAABHWYS7AjEfQZkjSrNw7JKMIUyH436kXYh2HpzhrkaE9jX5_WFUmQivSW_oA_aem_qRttjfua-CNfRG5NGm6Yag"  # Replace with the URL you want to monitor
+CHAT_ID = "922909522"
+WEBSITE_URL = "https://expressedzshop.peasy.top/body-scale-03-ar.html?fbclid=IwY2xjawE9_jpleHRuA2FlbQIxMAABHWYS7AjEfQZkjSrNw7JKMIUyH436kXYh2HpzhrkaE9jX5_WFUmQivSW_oA_aem_qRttjfua-CNfRG5NGm6Yag"
+
 def check_website(url):
-    """Check if the website is reachable."""
     try:
         response = requests.get(url, timeout=10)
         return response.status_code == 200
-    except requests.RequestException:
+    except requests.RequestException as e:
+        print(f"Error checking website: {e}")
         return False
 
 def send_telegram_message(token, chat_id, message):
-    """Send a message to a Telegram chat."""
     bot = Bot(token=token)
     bot.send_message(chat_id=chat_id, text=message)
 
 def main():
-    print("Starting script...")
     website_status = check_website(WEBSITE_URL)
     status_message = "up" if website_status else "down"
     message = f"Alert: The website {WEBSITE_URL} is {status_message}!"
     send_telegram_message(TELEGRAM_TOKEN, CHAT_ID, message)
 
 if __name__ == "__main__":
+    print("Starting script...")
     main()
