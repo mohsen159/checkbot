789eccc840bf34ed2f333b6f595945394c4d9673
mohsen159 <108313366+mohsen159@users.noreply.github.com>
2024-08-30 03:26:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import requests
 from telegram import Bot
+from telegram.ext import ApplicationBuilder, ContextTypes
+import asyncio
 
+# Telegram bot token and chat ID
 TELEGRAM_TOKEN = "7487022753:AAFPHijXrk4EqTmNJxhHTT6MllmTHog5Ue8"
 CHAT_ID = "922909522"
+
+# Website URL to check
 WEBSITE_URL = "https://expressedzshop.peasy.top/body-scale-03-ar.html?fbclid=IwY2xjawE9_jpleHRuA2FlbQIxMAABHWYS7AjEfQZkjSrNw7JKMIUyH436kXYh2HpzhrkaE9jX5_WFUmQivSW_oA_aem_qRttjfua-CNfRG5NGm6Yag"
+
+async def send_telegram_message(token, chat_id, message):
+    bot = Bot(token=token)
+    await bot.send_message(chat_id=chat_id, text=message)
 
 def check_website(url):
     try:
@@ -13,16 +22,12 @@ def check_website(url):
         print(f"Error checking website: {e}")
         return False
 
-def send_telegram_message(token, chat_id, message):
-    bot = Bot(token=token)
-    bot.send_message(chat_id=chat_id, text=message)
-
-def main():
+async def main():
     website_status = check_website(WEBSITE_URL)
     status_message = "up" if website_status else "down"
     message = f"Alert: The website {WEBSITE_URL} is {status_message}!"
-    send_telegram_message(TELEGRAM_TOKEN, CHAT_ID, message)
+    await send_telegram_message(TELEGRAM_TOKEN, CHAT_ID, message)
 
 if __name__ == "__main__":
     print("Starting script...")
-    main()
+    asyncio.run(main())
